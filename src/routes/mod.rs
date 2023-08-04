6516e80e6b43d69015f5ac4f122e2accd8ddfbeb
Alexiy Rybin <0xtapnisu@gmail.com>
2023-08-04 19:38:41 +0700

    Added cors

--- src/routes/mod.rs
+++ src/routes/mod.rs
@@ -2,14 +2,20 @@ mod convert;
 mod convert_raw;
 mod root;
 
-use axum::{routing::post, Router};
+use axum::{http::Method, routing::post, Router};
 use convert::convert;
 use convert_raw::convert_raw;
 use root::root;
+use tower_http::cors::{Any, CorsLayer};
 
 pub fn create_routes() -> Router {
+    let cors = CorsLayer::new()
+        .allow_methods([Method::GET, Method::POST])
+        .allow_origin(Any);
+
     Router::new()
         .route("/", post(root))
         .route("/convert", post(convert))
         .route("/convert/raw", post(convert_raw))
+        .layer(cors)
 }

123f5a405f7acd850e45b58dc21478b445c34eaa
Aleksei Rybin <0xalekseirybin@gmail.com>
2023-09-02 14:06:48 +0700

    import create_v1_routes

--- src/routes/mod.rs
+++ src/routes/mod.rs
@@ -1,6 +1,6 @@
 mod api;
 
-use api::create_api_routes;
+use api::{create_api_routes, v1::create_v1_routes};
 use axum::{http::Method, Router};
 use tower_http::cors::{Any, CorsLayer};
 
@@ -9,7 +9,7 @@ pub fn create_routes() -> Router {
         .allow_methods([Method::GET, Method::POST])
         .allow_origin(Any);
 
-    let v1 = api::v1::create_v1_routes();
+    let v1 = create_v1_routes();
     let api = create_api_routes();
 
     Router::new().nest("/", v1).nest("/api", api).layer(cors)

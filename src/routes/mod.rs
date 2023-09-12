63c15d8027d33e4940917e363019c575ec494f13
Aleksei Rybin <0xalekseirybin@gmail.com>
2023-09-12 16:56:15 +0700

    Added github api redirect

--- src/routes/mod.rs
+++ src/routes/mod.rs
@@ -1,7 +1,7 @@
 mod api;
 
 use api::{create_api_routes, v1::create_v1_routes};
-use axum::{http::Method, Router};
+use axum::{http::Method, response::Redirect, routing::get, Router};
 use tower_http::cors::{Any, CorsLayer};
 
 pub fn create_routes() -> Router {
@@ -12,5 +12,12 @@ pub fn create_routes() -> Router {
     let v1 = create_v1_routes();
     let api = create_api_routes();
 
-    Router::new().nest("/", v1).nest("/api", api).layer(cors)
+    Router::new()
+        .route(
+            "/",
+            get(|| async { Redirect::permanent("https://github.com/tapciify/api") }),
+        )
+        .nest("/", v1)
+        .nest("/api", api)
+        .layer(cors)
 }

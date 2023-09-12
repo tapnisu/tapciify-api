c9b7ee36b453f0bb5c0d31245f93445d5003be29
Aleksei Rybin <0xalekseirybin@gmail.com>
2023-09-12 16:54:20 +0700

    Added port variable

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ pub mod query;
 pub mod routes;
 
 use routes::create_routes;
+use std::env;
 use std::net::SocketAddr;
 
 #[tokio::main]
@@ -10,7 +11,12 @@ async fn main() {
 
     let app = create_routes();
 
-    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+    let addr = SocketAddr::from((
+        [127, 0, 0, 1],
+        env::var("PORT")
+            .map(|port| port.parse().unwrap())
+            .unwrap_or(3000),
+    ));
     tracing::debug!("listening on {}", addr);
     axum::Server::bind(&addr)
         .serve(app.into_make_service())

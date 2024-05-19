59e1ff41af9ff86e24974899ca27238956ae4bd4
Aleksei Rybin <0xalekseirybin@gmail.com>
2024-05-19 22:31:42 +0700

    Added tracing

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,67 @@
-use std::env;
+use axum::{extract::MatchedPath, http::Request};
+use std::{env, net::SocketAddr, time::Duration};
+use tokio::signal;
+use tower_http::trace::TraceLayer;
 
 #[tokio::main]
 async fn main() {
-    tracing_subscriber::fmt::init();
+    tracing_subscriber::fmt()
+        .with_max_level(tracing::Level::DEBUG)
+        .init();
 
-    let app = tapciify_api::create_routes();
+    let app = tapciify_api::create_routes().layer(TraceLayer::new_for_http().make_span_with(
+        |request: &Request<_>| {
+            let matched_path = request
+                .extensions()
+                .get::<MatchedPath>()
+                .map(MatchedPath::as_str);
+
+            tracing::info_span!(
+                "http_request",
+                method = ?request.method(),
+                matched_path,
+                some_other_field = tracing::field::Empty,
+            )
+        },
+    ));
+
+    let handle = axum_server::Handle::new();
+    let shutdown_future = shutdown_signal(handle.clone());
 
     let port = env::var("PORT").map_or(3000, |port| port.parse().unwrap());
-    let listener = tokio::net::TcpListener::bind(format!("0.0.0.0:{}", port))
+    let addr = SocketAddr::from(([127, 0, 0, 1], port));
+
+    let listener = tokio::net::TcpListener::bind(addr).await.unwrap();
+    tracing::debug!("listening on {}", listener.local_addr().unwrap());
+    axum::serve(listener, app)
+        .with_graceful_shutdown(shutdown_future)
         .await
         .unwrap();
-    axum::serve(listener, app).await.unwrap();
+}
+
+async fn shutdown_signal(handle: axum_server::Handle) {
+    let ctrl_c = async {
+        signal::ctrl_c()
+            .await
+            .expect("failed to install Ctrl+C handler");
+    };
+
+    #[cfg(unix)]
+    let terminate = async {
+        signal::unix::signal(signal::unix::SignalKind::terminate())
+            .expect("failed to install signal handler")
+            .recv()
+            .await;
+    };
+
+    #[cfg(not(unix))]
+    let terminate = std::future::pending::<()>();
+
+    tokio::select! {
+        _ = ctrl_c => {},
+        _ = terminate => {},
+    }
+
+    tracing::info!("Received termination signal shutting down");
+    handle.graceful_shutdown(Some(Duration::from_secs(10)));
 }
